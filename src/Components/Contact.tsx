b9df5a37e8c943ec09236dd755be22962ff65cc8
Himanshu Rawat <149815040+HimanshuRawat77@users.noreply.github.com>
2024-11-14 21:46:32 +0530

    Update Contact.tsx

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,41 +1,82 @@
-import { FormEvent, useRef } from "react";
+'use client'
+
+import { FormEvent, useRef, useState } from "react";
 import { Button, useMatches } from "@mantine/core";
 import emailjs from "@emailjs/browser";
 import { IconArrowRight, IconTopologyStar3 } from "@tabler/icons-react";
 import toast from "react-hot-toast";
+ 
 
-// .sendForm(
-//   "service_jjlxxjg",
-//   "template_61ji0c7",
-//   form.current,
-//   "PDptF5UqaqOi7hAdk"
+const validateForm = (id: string, value: any) => {
+  switch (id) {
+    case "name":
+      if (!value) return "Name is required";
+      if (value.length < 3) return "Name must be at least 3 characters long";
+      return "";
+    case "email":
+      if (!value) return "Email is required";
+      const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+      if (!emailRegex.test(value)) return "Email is invalid";
+      return "";
+    case "phone":
+      if (!value) return "Phone Number is required";
+      const phoneRegex = /^[0-9]{10}$/;
+      if (!phoneRegex.test(value)) return "Phone Number must be 10 digits long";
+      return "";
+    case "message":
+      if (!value) return "Message is required";
+      return "";
+    default:
+      return "";
+  }
+};
 
 const Contact = () => {
   const form = useRef<HTMLFormElement>(null);
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
 
     if (form.current) {
-      emailjs
-        .sendForm(
-          "service_jjlxxjg",
-          "template_61ji0c7",
-          form.current,
-          "PDptF5UqaqOi7hAdk"
-        )
-        .then(
-          (result) => {
-            toast.success("Submitted Successfully!", { duration: 4000 });
-            form.current?.reset();
-          },
-          (error) => {
-            console.log(error.text);
-            toast.error("Failed to send message, please try again", {
-              duration: 4000,
-            });
-          }
-        );
+      const formData = new FormData(form.current);
+      const newErrors: { [key: string]: string } = {};
+      const fields = ["name", "email", "phone", "message"];
+
+      fields.forEach((field) => {
+        const value = formData.get(field);
+        const error = validateForm(field, value);
+        if (error) {
+          newErrors[field] = error;
+        }
+      });
+
+      setErrors(newErrors);
+
+      if (Object.keys(newErrors).length === 0) {
+        emailjs
+          .sendForm(
+            "service_jjlxxjg",
+            "template_61ji0c7",
+            form.current,
+            "PDptF5UqaqOi7hAdk"
+          )
+          .then(
+            (result) => {
+              toast.success("Submitted Successfully!", { duration: 4000 });
+              form.current?.reset();
+              setErrors({});
+            },
+            (error) => {
+              console.log(error.text);
+              toast.error("Failed to send message, please try again", {
+                duration: 4000,
+              });
+            }
+          );
+      } else {
+        toast.error("Please correct the errors in the form", { duration: 4000 });
+      }
     } else {
       toast.error("Form reference is missing", { duration: 4000 });
     }
@@ -70,35 +111,50 @@ const Contact = () => {
           onSubmit={handleSubmit}
           className="w-[70%] lg-mx:w-full m-auto flex flex-col gap-6 sm-mx:p-4"
         >
-          <input
-            type="text"
-            name="name"
-            className={`block px-2.5 pb-2.5 pt-4 w-full text-xl sm-mx:text-lg xs-mx:text-base sm-mx:pb-1 sm-mx:pt-2 rounded-xl text-white bg-transparent border  border-textColor appearance-none hover:shadow-[0_0_8px_0_#64FFDA70] focus:shadow-[0_0_8px_0_#64FFDA70] focus:outline-none focus:ring-0 focus:border-primaryColor peer`}
-            placeholder="Name"
-            required={true}
-          />
-          <input
-            type="email"
-            name="email"
-            className={`block px-2.5 pb-2.5 pt-4 w-full text-xl sm-mx:text-lg xs-mx:text-base sm-mx:pb-1 sm-mx:pt-2 rounded-xl text-white bg-transparent border  border-textColor appearance-none hover:shadow-[0_0_8px_0_#64FFDA70] focus:shadow-[0_0_8px_0_#64FFDA70] focus:outline-none focus:ring-0 focus:border-primaryColor peer`}
-            placeholder="Email"
-            required={true}
-          />
-          <input
-            type="number"
-            name="phone"
-            className={`block px-2.5 pb-2.5 pt-4 w-full text-xl sm-mx:text-lg xs-mx:text-base sm-mx:pb-1 sm-mx:pt-2 rounded-xl text-white bg-transparent border  border-textColor appearance-none hover:shadow-[0_0_8px_0_#64FFDA70] focus:shadow-[0_0_8px_0_#64FFDA70] focus:outline-none focus:ring-0 focus:border-primaryColor peer`}
-            placeholder="Phone Number"
-            required={true}
-          />
-
-          <textarea
-            name="message"
-            rows={4}
-            className={`block px-2.5 pb-2.5 xs-mx:text-base pt-4 w-full text-xl sm-mx:text-lg rounded-xl text-white bg-transparent border border-textColor appearance-none hover:shadow-[0_0_8px_0_#64FFDA70] focus:shadow-[0_0_8px_0_#64FFDA70] focus:outline-none focus:ring-0 focus:border-primaryColor peer`}
-            placeholder="Message"
-            required={true}
-          ></textarea>
+          <div>
+            <input
+              type="text"
+              name="name"
+              className={`block px-2.5 pb-2.5 pt-4 w-full text-xl sm-mx:text-lg xs-mx:text-base sm-mx:pb-1 sm-mx:pt-2 rounded-xl text-white bg-transparent border ${
+                errors.name ? "border-red-500" : "border-textColor"
+              } appearance-none hover:shadow-[0_0_8px_0_#64FFDA70] focus:shadow-[0_0_8px_0_#64FFDA70] focus:outline-none focus:ring-0 focus:border-primaryColor peer`}
+              placeholder="Name"
+            />
+            {errors.name && <p className="text-red-500 text-sm mt-1">{errors.name}</p>}
+          </div>
+          <div>
+            <input
+              type="email"
+              name="email"
+              className={`block px-2.5 pb-2.5 pt-4 w-full text-xl sm-mx:text-lg xs-mx:text-base sm-mx:pb-1 sm-mx:pt-2 rounded-xl text-white bg-transparent border ${
+                errors.email ? "border-red-500" : "border-textColor"
+              } appearance-none hover:shadow-[0_0_8px_0_#64FFDA70] focus:shadow-[0_0_8px_0_#64FFDA70] focus:outline-none focus:ring-0 focus:border-primaryColor peer`}
+              placeholder="Email"
+            />
+            {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
+          </div>
+          <div>
+            <input
+              type="tel"
+              name="phone"
+              className={`block px-2.5 pb-2.5 pt-4 w-full text-xl sm-mx:text-lg xs-mx:text-base sm-mx:pb-1 sm-mx:pt-2 rounded-xl text-white bg-transparent border ${
+                errors.phone ? "border-red-500" : "border-textColor"
+              } appearance-none hover:shadow-[0_0_8px_0_#64FFDA70] focus:shadow-[0_0_8px_0_#64FFDA70] focus:outline-none focus:ring-0 focus:border-primaryColor peer`}
+              placeholder="Phone Number"
+            />
+            {errors.phone && <p className="text-red-500 text-sm mt-1">{errors.phone}</p>}
+          </div>
+          <div>
+            <textarea
+              name="message"
+              rows={4}
+              className={`block px-2.5 pb-2.5 xs-mx:text-base pt-4 w-full text-xl sm-mx:text-lg rounded-xl text-white bg-transparent border ${
+                errors.message ? "border-red-500" : "border-textColor"
+              } appearance-none hover:shadow-[0_0_8px_0_#64FFDA70] focus:shadow-[0_0_8px_0_#64FFDA70] focus:outline-none focus:ring-0 focus:border-primaryColor peer`}
+              placeholder="Message"
+            ></textarea>
+            {errors.message && <p className="text-red-500 text-sm mt-1">{errors.message}</p>}
+          </div>
 
           <Button
             fullWidth
@@ -117,4 +173,5 @@ const Contact = () => {
     </div>
   );
 };
+
 export default Contact;
